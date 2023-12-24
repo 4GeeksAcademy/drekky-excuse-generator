afaa898371cd0e342d0498463b3ddc3cacd66901
drekky <130186719+drekky@users.noreply.github.com>
2023-12-24 00:13:27 -0500

    Moved index.html, style.css, App.js to src folder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const whoArray = ["I", "My pet", "My friend", "A mysterious stranger"];
+const actionArray = ["forgot to", "had to", "couldn't", "chose not to"];
+const whatArray = ["set my alarm", "attend the meeting", "finish my homework", "go to the gym"];
+const whenArray = ["this morning", "yesterday", "last night", "a few minutes ago"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomElement(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateExcuse() {
+  const who = getRandomElement(whoArray);
+  const action = getRandomElement(actionArray);
+  const what = getRandomElement(whatArray);
+  const when = getRandomElement(whenArray);
+
+  const excuseElement = document.getElementById('excuse');
+  excuseElement.textContent = `${who} ${action} ${what} ${when}.`;
+
+}
